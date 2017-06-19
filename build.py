832dedf888cf34f6656fb9bf11ee87d2c0c2cc24
arunabh15091989 <Arunabh.social@gmail.com>
2017-06-19 15:32:06 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,7 @@
 def solution(list_of_nums):
     """Enter Code Here"""
+    Even = []
+    Even = filter(lambda x: x%2==0 , list_of_nums)
+    Odd = []
+    Odd = filter(lambda x: x%2!=0, list_of_nums)
+    return {'EVEN':len(Even),'ODD':len(Odd)}
